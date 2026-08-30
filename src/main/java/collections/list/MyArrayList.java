93f9e335caa7a1ad1fc783b845d000d9b80212c1
DevAbdul001 <abdurahmanali363@gmail.com>
2026-08-30 22:14:40 +0300

    feat : add get(index) feature

--- src/main/java/collections/list/MyArrayList.java
+++ src/main/java/collections/list/MyArrayList.java
@@ -1,5 +1,7 @@
 package collections.list;
 
+import java.util.*;
+
 public class MyArrayList<E> implements MyList<E> {
 
   private Object[] elements;
@@ -34,7 +36,7 @@ public class MyArrayList<E> implements MyList<E> {
   public void add(int index, E element) {
 
     if (index < 0 || index > size) {
-      throw new IndexOutOfBoundsException("index" + index + "size" + size);
+      throw new IndexOutOfBoundsException("index" + index + ", " + "size" + size);
     }
 
     if (size + 1 > elements.length) {
@@ -48,70 +50,95 @@ public class MyArrayList<E> implements MyList<E> {
     System.arraycopy(elements, index, elements, index + 1, size - index);
     elements[index] = element;
     size++;
-  
   }
 
   // ===================================================================================================
 
   boolean addAll(Collection<? extends E> col) {
-	  if(col.size() > elements.length - size) {
-		  Object [] newElements = new Object[elements.length + col.size()];
+    if (col.size() > elements.length - size) {
+      Object[] newElements = new Object[elements.length + col.size()];
 
-		  for(int i = 0; i < elements.length; i++){
-			  newElements[i] = elements[i];
-		  }
-		  elements = newElements;
-	  }
+      for (int i = 0; i < elements.length; i++) {
+        newElements[i] = elements[i];
+      }
+      elements = newElements;
+    }
 
-	  for(E element : col) {
-		elements[size] = element;
-		size++;
-	  }
-	  return true;
+    for (E element : col) {
+      elements[size] = element;
+      size++;
+    }
+    return true;
   }
 
   // ====================================================================================================
-  void clear(){
-	Object[] elements = new Object[0];
+  void clear() {
+    Object[] newElements = new Object[0];
+    elements = newElements;
   }
 
   // ===================================================================================================
-  boolean contains (Object object){
+  boolean contains(Object object) {
 
-	for(int i = 0; i < elements.length; i++){
-		if( elements[i] == object){
-			return true;	
-		}
-	
-	}
+    for (int i = 0; i < elements.length; i++) {
+      if (elements[i] == object) {
+        return true;
+      }
+    }
   }
 
   // ===================================================================================================
-  boolean containsAll(Collection<?> col){
-	
-	for(E element : col) {
-		boolean matches = false;
+  boolean containsAll(Collection<?> col) {
 
-		for(int i = 0; i<elements.size; i++){
-			if( elements[i] == element ) {
-				matches = true;
-			} 
-		}
+    for (E element : col) {
+      boolean matches = false;
 
-		if( matches == false ) {
-			return false;
-		}
-	}
+      for (int i = 0; i < elements.size; i++) {
+        if (elements[i] == element) {
+          matches = true;
+        }
+      }
 
-	return true;
+      if (matches == false) {
+        return false;
+      }
+    }
 
+    return true;
   }
 
+  // ===================================================================================================
+  boolean equals(Object object) {
 
+    // check if its a List
+    if (!(object instanceof List)) {
+      return false;
+    }
 
+    // cast object to a list
+    List<?> cast = (List<?>) object;
 
+    // check for size
+    if (cast.size() != elements.size) {
+      return false;
+    }
 
+    // loop to check each element's index and value
+    for (int i = 0; i < elements.size; i++) {
 
+      if (!Objects.equals(elements[i], cast.get(i))) {
+        return false;
+      }
+    }
 
+    return true;
+  }
 
+  // ===================================================================================================
+  E get(int index) {
+    if (index >= size || index < 0) {
+      throw new IndexOutOfBoundsException("index: " + index + ", " + "size: " + size);
+    }
+    return elements[index];
+  }
 }
